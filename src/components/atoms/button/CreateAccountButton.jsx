17473be828f76742d1ec8346e04dab788620b43c
kei-prog <kei08prog@gmail.com>
2023-12-04 17:44:55 +0900

    createaccountbutton.jsxの追加

---
+++ src/components/atoms/button/CreateAccountButton.jsx
@@ -0,0 +1,9 @@
+const CreateAccountButton = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-60 py-2 rounded-full">
+      アカウントを作成
+    </button>
+  );
+};
+
+export default CreateAccountButton;
