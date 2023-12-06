95839d23b8fb756bd9ba60a5fa724482f1223fb6
kei-prog <kei08prog@gmail.com>
2023-12-07 03:05:12 +0900

    routes設定の追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Top from "./routes/Top";
+import NoMatch from "./routes/NoMatch";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/top" element={<Top />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
