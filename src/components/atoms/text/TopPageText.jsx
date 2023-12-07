9151653b61331189a39a0c0d4377621cb5d00777
kei-prog <kei08prog@gmail.com>
2023-12-08 04:39:30 +0900

    トップページテキストの追加

---
+++ src/components/atoms/text/TopPageText.jsx
@@ -0,0 +1,12 @@
+const TopPageText = () => {
+  return (
+    <div>
+      <h1 className="text-7xl text-left font-bold">すべての話題が、ここに。</h1>
+      <h1 className="text-4xl text-left font-bold mt-6">
+        今すぐ参加しましょう。
+      </h1>
+    </div>
+  );
+};
+
+export default TopPageText;
