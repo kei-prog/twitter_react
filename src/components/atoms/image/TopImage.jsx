409b8357133ca9b693d940833996b9f9fb00d336
kei-prog <kei08prog@gmail.com>
2023-12-08 04:40:41 +0900

    トップイメージの追加

---
+++ src/components/atoms/image/TopImage.jsx
@@ -0,0 +1,11 @@
+import Image from "../../../assets/twitter-top.jpg";
+
+const TopImage = () => {
+  return (
+    <div>
+      <img src={Image} alt="random" className="w-full" />
+    </div>
+  );
+};
+
+export default TopImage;
