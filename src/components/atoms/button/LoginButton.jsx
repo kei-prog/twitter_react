89ca4ad6576063360b549951f27f060699a53391
kei-prog <kei08prog@gmail.com>
2024-01-06 22:57:31 +0900

    ログインボタンの修正

--- src/components/atoms/button/LoginButton.jsx
+++ src/components/atoms/button/LoginButton.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import LoginModal from "../../organisms/LoginModal";
+
 const LoginButton = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <button className="bg-black border-white hover:bg-gray-950 text-white font-bold w-60 py-2 rounded-full">
-      ログイン
-    </button>
+    <>
+      <button
+        className="bg-black border-white hover:bg-gray-950 text-white
+                   font-bold w-60 py-2 rounded-full"
+        onClick={handleOpenModal}
+      >
+        ログイン
+      </button>
+
+      <LoginModal isOpen={isModalOpen} onClose={handleCloseModal} />
+    </>
   );
 };
 
