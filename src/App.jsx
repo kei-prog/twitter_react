6d1552d7414ba1aac17526ad1d21163e18a5f452
kei-prog <kei08prog@gmail.com>
2024-01-07 00:15:47 +0900

    routesの追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Top from "./routes/Top";
 import NoMatch from "./routes/NoMatch";
+import TweetList from "./routes/TweetList";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Top />} />
         <Route path="/top" element={<Top />} />
+        <Route path="/index" element={<TweetList />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
