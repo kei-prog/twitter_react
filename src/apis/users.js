e48cddf4b063eda66cc91322f9b9e738ee341cd0
kei-prog <kei08prog@gmail.com>
2024-01-10 02:07:27 +0900

    ユーザ認証の追加

--- src/apis/users.js
+++ src/apis/users.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { USERS_REGISTRATIONS } from "../urls/index";
 import { USERS_SESSIONS } from "../urls/index";
+import { USERS_VALIDATE_TOKEN } from "../urls/index";
 
 export const postUserRegistration = async (userData) => {
   try {
@@ -37,5 +38,20 @@ export const postUserSignIn = async (userData) => {
       success: false,
       errors: errorMessages,
     };
+  }
+};
+
+export const validateUserToken = async () => {
+  try {
+    await axios.get(USERS_VALIDATE_TOKEN, {
+      withCredentials: true,
+    });
+    return { success: true };
+  } catch (e) {
+    const errorMessages =
+      e.response && e.response.data && e.response.data.errors
+        ? e.response.data.errors
+        : ["トークンの検証に失敗しました。"];
+    return { success: false, errors: errorMessages };
   }
 };
