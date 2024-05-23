34432a26a0a21b6d199f453f222b992f21afa9d1
kei-prog <kei08prog@gmail.com>
2024-05-23 23:57:29 +0900

    画像プレビューの追加

---
+++ src/components/atoms/field/ImagePreview.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ImagePreview = ({ previews, handleRemoveImage }) => {
+  return (
+    <div className="mt-2 grid grid-cols-2 gap-2">
+      {previews.map((preview, index) => (
+        <div key={index} className="relative">
+          <img
+            src={preview}
+            alt={`Preview ${index + 1}`}
+            className="w-full h-auto"
+          />
+          <button
+            type="button"
+            onClick={() => handleRemoveImage(index)}
+            className="absolute top-0 right-0 w-6 h-6 text-white bg-gray-500 rounded-full"
+          >
+            ×
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ImagePreview;
