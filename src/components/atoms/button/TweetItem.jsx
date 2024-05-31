642a510357e619776383b991a548b54b82f61fd6
kei-prog <kei08prog@gmail.com>
2024-06-01 08:59:51 +0900

    ツイートクリックで詳細に遷移するように修正

--- src/components/atoms/button/TweetItem.jsx
+++ src/components/atoms/button/TweetItem.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import TimeAgo from "../text/TimeAgo";
 
 const TweetItem = ({ item }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/tweet/${item.id}`);
+  };
+
   return (
-    <div key={item.id} className="h-24 p-4 border border-gray-800">
+    <div
+      key={item.id}
+      className="h-24 p-4 border border-gray-800 cursor-pointer"
+      onClick={handleClick}
+    >
       <div className="font-bold text-left">
         {item.user.name} <TimeAgo dateString={item.created_at} />
       </div>
