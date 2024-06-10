6d17b84a65358b80f67c4298065879fb9efab390
kei-prog <kei08prog@gmail.com>
2024-06-11 03:05:22 +0900

    確認メッセージコンポーネントの追加

---
+++ src/components/atoms/field/ConfirmationModal.jsx
@@ -0,0 +1,29 @@
+const ConfirmationModal = ({ onClose, onConfirm }) => (
+  <div
+    onClick={onClose}
+    className="fixed inset-0 z-50 flex items-center justify-center bg-gray-500/50"
+  >
+    <div
+      onClick={(e) => e.stopPropagation()}
+      className="p-6 text-center bg-black rounded-lg shadow-lg"
+    >
+      <p className="mb-4 text-xl font-bold">ポストを削除しますか?</p>
+      <div className="flex flex-col justify-center overflow-y-hidden">
+        <button
+          className="px-4 py-2 mr-2 text-white bg-red-700 rounded-full"
+          onClick={onConfirm}
+        >
+          削除
+        </button>
+        <button
+          onClick={onClose}
+          className="px-4 py-2 mt-4 text-white bg-black border border-white rounded-full"
+        >
+          キャンセル
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default ConfirmationModal;
