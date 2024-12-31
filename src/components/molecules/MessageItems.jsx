37c3c9f54ebbaef23314f64c9049094b7573eba5
kei-prog <kei08prog@gmail.com>
2024-12-31 16:59:57 +0900

    MessageItemsの追加

---
+++ src/components/molecules/MessageItems.jsx
@@ -0,0 +1,28 @@
+import InfiniteScroll from "react-infinite-scroll-component";
+import MessageItem from "../atoms/button/MessageItem";
+
+const MessageItems = ({
+  messageItems,
+  messageHasMore,
+  fetchMoreMessageData,
+}) => {
+  return (
+    <div className="border-b border-gray-800 flex-1">
+      <InfiniteScroll
+        dataLength={messageItems.length}
+        next={fetchMoreMessageData}
+        hasMore={messageHasMore}
+        height={"90vh"}
+        loader={<h4>Loading...</h4>}
+        inverse={true}
+        className="flex flex-col-reverse"
+      >
+        {messageItems.map((messageItem) => (
+          <MessageItem key={messageItem.id} item={messageItem} />
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+export default MessageItems;
