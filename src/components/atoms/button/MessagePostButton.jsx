2a843476a457e1065e3721341415d961e50fab7a
kei-prog <kei08prog@gmail.com>
2025-01-07 21:25:54 +0900

    MessagePostButtonの追加

---
+++ src/components/atoms/button/MessagePostButton.jsx
@@ -0,0 +1,17 @@
+import MessagePostIcon from "/src/assets/message-post.svg?react";
+
+const MessagePostButton = ({ isDisabled }) => {
+  return (
+    <button
+      type="submit"
+      className={`rounded-full w-8 h-8 hover:bg-gray-700 border-none flex items-center justify-center`}
+      disabled={isDisabled}
+    >
+      <MessagePostIcon
+        className={`w-5 h-5 ${isDisabled ? "text-blue-700" : "text-blue-500"}`}
+      />
+    </button>
+  );
+};
+
+export default MessagePostButton;
