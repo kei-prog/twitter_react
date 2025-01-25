aed822452f966bfc4a7d88e0ffa994e9003c2d37
kei-prog <kei08prog@gmail.com>
2025-01-25 09:04:25 +0900

    SideMenu 通知ボタン実装

--- src/components/molecules/SideMenu.jsx
+++ src/components/molecules/SideMenu.jsx
@@ -11,6 +11,9 @@ const SideMenu = () => {
   const handleGroup = () => {
     navigate("/group");
   };
+  const handleBookmark = () => {
+    navigate("/bookmark");
+  };
 
   return (
     <div className="sticky top-0 w-16 h-screen pt-4 lg:w-72">
@@ -24,42 +27,66 @@ const SideMenu = () => {
           className="flex mb-6 text-xl text-left cursor-pointer"
           onClick={handleHome}
         >
-          <img
-            src="/src/assets/home.svg"
-            alt="home"
-            className="w-6 h-6 rounded-full"
-          />
-          <span className="hidden lg:block ms-4">ホーム</span>
+          <button
+            className="flex focus:outline-none"
+            onClick={handleBookmark}
+            aria-label="ホームボタン"
+          >
+            <img
+              src="/src/assets/home.svg"
+              alt="home"
+              className="w-6 h-6 rounded-full"
+            />
+            <span className="hidden lg:block ms-4">ホーム</span>
+          </button>
         </li>
         <li
           className="flex mb-6 text-xl text-left cursor-pointer"
           onClick={handleNotification}
         >
-          <img
-            src="/src/assets/notification.svg"
-            alt="notification"
-            className="flex w-6 h-6 rounded-full"
-          />
-          <span className="hidden lg:block ms-4">通知</span>
+          <button
+            className="flex focus:outline-none"
+            onClick={handleBookmark}
+            aria-label="通知ボタン"
+          >
+            <img
+              src="/src/assets/notification.svg"
+              alt="notification"
+              className="flex w-6 h-6 rounded-full"
+            />
+            <span className="hidden lg:block ms-4">通知</span>
+          </button>
         </li>
         <li
           className="flex mb-6 text-xl text-left cursor-pointer"
           onClick={handleGroup}
         >
-          <img
-            src="/src/assets/message.svg"
-            alt="message"
-            className="flex w-6 h-6 rounded-full"
-          />
-          <span className="hidden lg:block ms-4">メッセージ</span>
+          <button
+            className="flex focus:outline-none"
+            onClick={handleBookmark}
+            aria-label="メッセージボタン"
+          >
+            <img
+              src="/src/assets/message.svg"
+              alt="message"
+              className="flex w-6 h-6 rounded-full"
+            />
+            <span className="hidden lg:block ms-4">メッセージ</span>
+          </button>
         </li>
         <li className="flex mb-6 text-xl text-left cursor-pointer">
-          <img
-            src="/src/assets/bookmark.svg"
-            alt="bookmark"
-            className="flex w-6 h-6 rounded-full"
-          />
-          <span className="hidden lg:block ms-4">ブックマーク</span>
+          <button
+            className="flex focus:outline-none"
+            onClick={handleBookmark}
+            aria-label="ブックマークボタン"
+          >
+            <img
+              src="/src/assets/bookmark.svg"
+              alt="bookmark"
+              className="flex w-6 h-6 rounded-full"
+            />
+            <span className="hidden lg:block ms-4">ブックマーク</span>
+          </button>
         </li>
         <li className="flex mb-6 text-xl text-left cursor-pointer">
           <img
